64309291757d6f0b266ec4f72af6906ff57d7328
Denkata-Iliev <denislav9711@gmail.com>
2019-04-09 00:09:21 +0300

    Pieces appear on board

--- src/gui/Board.java
+++ src/gui/Board.java
@@ -4,8 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Board {
-    private static final Dimension FRAME_DIMENSION = new Dimension(800, 800);
-    private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
+    private static final Dimension FRAME_DIMENSION = new Dimension(700, 700);
+    private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(500, 450);
+    private static final int ROW_LENGTH = 8;
+    private static final int ROWS_NUMBER = 8;
 
     private JFrame gameFrame = new JFrame("Chess");
     private MenuBar menuBar = new MenuBar();
@@ -17,10 +19,10 @@ public class Board {
     private void showFrame() {
         gameFrame.setSize(FRAME_DIMENSION);
         gameFrame.setJMenuBar(menuBar);
-        gameFrame.setVisible(true);
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.setLocationRelativeTo(null);
         drawBoard();
+        gameFrame.setVisible(true);
     }
 
     private void drawBoard() {
@@ -32,18 +34,17 @@ public class Board {
 
         BoardPanel() {
             super(new GridLayout(8, 8));
-            setVisible(true);
             addTileAndAssignTileColor();
             setPreferredSize(BOARD_PANEL_DIMENSION);
+            setVisible(true);
+            validate();
         }
 
         private void addTileAndAssignTileColor() {
             TilePanel tilePanel;
-            for (int row = 0; row < BoardUtils.ROWS_NUMBER; row++) {
-                for (int tileId = 0; tileId < BoardUtils.ROW_LENGTH; tileId++) {
-                    tilePanel = new TilePanel();
-                    tilePanel.paintTile(row, tileId);
-                    tilePanel.assignPieceIcon();
+            for (int row = 1; row <= ROWS_NUMBER; row++) {
+                for (int tileId = 1; tileId <= ROW_LENGTH; tileId++) {
+                    tilePanel = new TilePanel(row, tileId);
                     add(tilePanel);
                 }
             }
--- src/gui/BoardUtils.java
+++
@@ -1,6 +0,0 @@
-package gui;
-
-public class BoardUtils {
-    public static final int ROW_LENGTH = 8;
-    public static final int ROWS_NUMBER = 8;
-}
